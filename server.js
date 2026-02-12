d1149e13244c02804be3c594548ce98dee2192f9
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2026-02-13 02:41:10 +0530

    fix:Add cors middleware for 'credentials: true'

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
